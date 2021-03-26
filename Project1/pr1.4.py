c36a66d17853082ae5cf758a5bca93a8f5dcfcde
M0NAKOV <monakow.andrew@yandex.ru>
2021-03-26 23:12:54 +0300

    pr1.4 commit

---
+++ Project1/pr1.4.py
@@ -0,0 +1,12 @@
+import math
+
+def f(n):
+    if (n == 0):
+        return 6
+    elif (n == 1):
+        return 6
+    else:
+        return (math.sin(f(n-2)) + 1/41*f(n-1))
+
+print('%.2e' % f(11))
+print('%.2e' % f(13))
